511a6dd2cb4b6b018201ae827e88f26582b5ffa0
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-09-06 11:09:36 -0300

    add data

--- src/app/components/list-personas/list-personas.component.ts
+++ src/app/components/list-personas/list-personas.component.ts
@@ -3,7 +3,12 @@ import { Persona } from 'src/app/interfaces/persona';
 
 
 const listPersonas: Persona[] = [
-  { nombre: "Tomas", apellido: "Perez", correo: "tperez@gmail.com", tipoDocumento: "DNI", documento: 35222111, fechaNacimiento: new Date()  },  
+  { nombre: "Tomas", apellido: "Perez", correo: "tperez@gmail.com", tipoDocumento: "DNI", documento: 35222111, fechaNacimiento: new Date()  },
+  { nombre: "Juan", apellido: "Juarez", correo: "jjuarez@gmail.com", tipoDocumento: "DNI", documento: 31222333, fechaNacimiento: new Date()  },  
+  { nombre: "Marcos", apellido: "Gonzalez", correo: "marcosgonzalez@gmail.com", tipoDocumento: "DNI", documento: 28976555, fechaNacimiento: new Date()  },  
+  { nombre: "Micaela", apellido: "Aliaga", correo: "maliaga@gmail.com", tipoDocumento: "DNI", documento: 27564235, fechaNacimiento: new Date()  },  
+  { nombre: "Fernanda", apellido: "Ortiz", correo: "fortiz@gmail.com", tipoDocumento: "DNI", documento: 33237543, fechaNacimiento: new Date()  },  
+  { nombre: "Sofia", apellido: "Pelegrini", correo: "spelegrini@gmail.com", tipoDocumento: "DNI", documento: 22334565, fechaNacimiento: new Date()  },  
 ];
 
 @Component({
